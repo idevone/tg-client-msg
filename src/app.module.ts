bb85a45ab2493277378e0a13d8227c8efc02ec4c
Yaroslav Khakhuda <hahyda14@gmail.com>
2024-08-16 00:46:15 +0300

    fix: cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -6,6 +6,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from './configs/config.module';
 import { PostgresConfig } from './configs/postgres.config';
 import { TelegramModule } from './telegram/telegram.module';
+import * as cors from 'cors';
+import { ConfigService } from './configs/config.service';
 
 @Module({
   imports: [
@@ -20,4 +22,19 @@ import { TelegramModule } from './telegram/telegram.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  private readonly ORIGIN_URL: string;
+  constructor(configService: ConfigService) {
+    this.ORIGIN_URL = configService.getString('ORIGIN_URL');
+  }
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cors({
+          origin: this.ORIGIN_URL, // Замініть на ваш домен
+          credentials: true, // Дозволяє передачу облікових даних
+        }),
+      )
+      .forRoutes('*');
+  }
+}
